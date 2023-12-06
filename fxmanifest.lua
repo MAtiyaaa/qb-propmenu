d89df9782105424fee15189c32adb4cbf929612a
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2023-12-06 22:50:08 +0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Atiya'
+description 'In-game Prop Offset Editor (Name is WIP)'
+version '0.0.1'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/bonenames.js',
+    'html/propnames.js',
+    'html/lib/select2.min.css',
+    'html/lib/select2.min.js',
+    'html/lib/jquery.js'
+}
